a6d7719df7606a74ed5a709e6f8df5af75b3a91b
Luiz Fernando Meneses Pereira <luizfernandomdp14@outlook.com>
2024-05-06 17:32:44 -0300

    ajustes

--- Site/nova-home/home.js
+++
@@ -1,9 +0,0 @@
-const myCarouselElement = document.querySelector('#carouselExampleAutoplaying')
-
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 100,
-  touch: false
-})
-carousel.addEventListener('slide.bs.carousel', event => {
-    console.log('oi')
-  })
